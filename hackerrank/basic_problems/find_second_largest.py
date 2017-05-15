a9ddc7baa68d5577d78ddc075dcf4f0acf0d8d28
Chhaya Choudhary <er.chhayachoudhary@gmail.com>
2017-05-15 14:54:07 -0700

    Added find second largest number program

--- hackerrank/basic_problems/find_second_largest.py
+++ hackerrank/basic_problems/find_second_largest.py
@@ -1,6 +1,25 @@
+# You are given N numbers. Store them in a list and find the second largest number.
+# 
+# Input Format 
+# The first line contains N. The second line contains an array A[] of N integers each separated by a space.
+# 
+# Output Format 
+# Output the value of the second largest number.
+# 
+# Sample Input
+# 5
+# 2 3 6 6 5
+# 
+# Sample Output
+# 5
+
+
+# First approach
 def find_second_largest():
-    n = int(input())
-    arr = list(map(int, input().split()))
+    # input() reads a line from input, converts it to a string (stripping a trailing newline).
+    n = int(input())      
+    # This below line reads a line, split it at whitespaces and applies int() to every element of the result.
+    arr = list(map(int, input().split()))   
     arr.sort(reverse = True)
     i = 0
     while(arr[i] == arr[i+1]):
@@ -9,3 +28,12 @@ def find_second_largest():
 
 if __name__ == '__main__':
     find_second_largest()
+
+
+# Second Approach
+if __name__ == '__main__':
+    n = int(input())    
+    arr = list(map(int, input().split()))
+    M = max(arr)
+
+    print(max(n for n in arr if n!=M))
